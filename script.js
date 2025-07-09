34336636ca9d22309911bdd8b6b090697b5788bd
Kanishkkumar-Git <kanishknew849@gmail.com>
2025-07-09 22:24:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-if(localStorage.getItem("name")){
-    a = localStorage.getItem("name")
-    document.write("Hii "+a)
-}
-else{
-let a = prompt("Enter your name")
-localStorage.setItem("name",a);
-}

66de788d78ddf29a328e16e730b8abe64bb27534
Kanishkkumar-Git <kanishknew849@gmail.com>
2025-07-10 10:12:06 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   const [videos, setVideos] = useState([]);
-
+const user = JSON.parse(localStorage.getItem('user'));
   useEffect(() => {
     axios.get('http://127.0.0.1:8000/api/videos/')
       .then(res => setVideos(res.data))
       .catch(err => console.error(err));
   }, []);
-
+ const toggleWatchLater = async (videoId) => {
+    if (!user || !user.token) {
+      alert("Please login to use Watch Later");
+      return;
+    }
   return (
     <div style={{ padding: '20px', marginLeft: '200px' }}>
       <h2>Welcome to Videos 📺</h2>
@@ -28,6 +32,7 @@ function Home() {
               Your browser does not support the video tag.
             </video>
             <p>{video.description}</p>
+    <button onClick={() => toggleWatchLater(video.id)}>➕ Watch Later</button>
            <Link to={`/videos/${video.id}`}>
   <button style={{ background: "blue", color: "white" }}>Watch</button>
 </Link>

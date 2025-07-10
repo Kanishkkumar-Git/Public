2221e6d57579440903e0803684034cbcbf2a1456
Kanishkkumar-Git <kanishknew849@gmail.com>
2025-07-10 10:07:41 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('videos/', views.list_videos),
     path('videos/<int:id>/', views.get_video),
     path('my-videos/', views.user_videos),
-path('my-videos/', views.user_videos),
+    path('my-videos/', views.user_videos),
+    path('watchlater/toggle/<int:video_id>/', views.toggle_watch_later),
+    path('watchlater/list/', views.list_watch_later),
+
 
 ]
